bce7f7b150201885782fcae703b520c13a0eb3c4
Parham Alvani <parham.alvani@gmail.com>
2017-01-17 06:24:35 +0330

    Question 2 was added.

---
+++ 02.m
@@ -0,0 +1,20 @@
+pkg load image
+
+im_path = input("Please enter image path: ", "s");
+
+im_source = imread(im_path);
+
+im_source = rgb2gray(im_source);
+im_source = im2double(im_source);
+
+printf ("Source image rank: %d\n", rank (im_source));
+
+k = input("Please enter k: ");
+
+[im_U, im_S, im_V] = svd (im_source);
+im_S(k+1:end,:) = 0;
+im_S(:,k+1:end) = 0;
+im_sink = im_U * sqrt(im_S) * sqrt(im_S) * im_V';
+
+im_path = input("Please enter save path: ", "s");
+imwrite(im_sink, im_path);
