f922c50ebe5720733355df9bcb7182a22f14b476
Parham Alvani <parham.alvani@gmail.com>
2017-01-18 04:32:26 +0330

    Question was improved.

--- 02.m
+++ 02.m
@@ -2,21 +2,23 @@ pkg load image
 
 clear
 
-im_path = input("Please enter image path: ", "s");
+im_path = input ("Please enter image path: ", "s");
 
-im_source = imread(im_path);
+im_source = imread (im_path);
 
 im_source = rgb2gray(im_source);
 im_source = im2double(im_source);
 
 printf ("Source image rank: %d\n", rank (im_source));
 
-k = input("Please enter k: ");
+k = input ("Please enter k: ");
 
 [im_U, im_S, im_V] = svd (im_source);
 im_S(k+1:end,:) = 0;
 im_S(:,k+1:end) = 0;
-im_sink = im_U * sqrt(im_S) * sqrt(im_S) * im_V';
+im_sink = im_U * im_S * im_V';
 
-im_path = input("Please enter save path: ", "s");
-imwrite(im_sink, im_path);
+im_path = input ("Please enter save path: ", "s");
+imwrite (im_sink, im_path);
+
+printf ("Mean Square Error: %g\n", mean (meansq (im_source - im_sink)));
