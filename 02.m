470025c24bf040452d00852353fefb5958eef176
Parham Alvani <parham.alvani@gmail.com>
2017-01-18 04:47:05 +0330

    Loop over different k !

--- 02.m
+++ 02.m
@@ -11,14 +11,18 @@ im_source = im2double(im_source);
 
 printf ("Source image rank: %d\n", rank (im_source));
 
-k = input ("Please enter k: ");
+ks = input ("Please enter k(s) [...]: ");
 
-[im_U, im_S, im_V] = svd (im_source);
-im_S(k+1:end,:) = 0;
-im_S(:,k+1:end) = 0;
-im_sink = im_U * im_S * im_V';
+for k = ks
+	printf (" -- k = %d -- \n", k);
 
-im_path = input ("Please enter save path: ", "s");
-imwrite (im_sink, im_path);
+	[im_U, im_S, im_V] = svd (im_source);
+	im_S(k+1:end,:) = 0;
+	im_S(:,k+1:end) = 0;
+	im_sink = im_U * im_S * im_V';
 
-printf ("Mean Square Error: %g\n", mean (meansq (im_source - im_sink)));
+	im_path = input ("Please enter save path: ", "s");
+	imwrite (im_sink, im_path);
+
+	printf ("Mean Square Error: %g\n", mean (meansq (im_source - im_sink)));
+endfor
